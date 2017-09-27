d3127c17edc10a9b1087cfc5584ae9661931eaf2
Shyar <shiyarxelil@hotmail.com>
2017-09-27 11:12:53 +0200

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,78 @@
+const startButton = document.querySelector("a");
+const screenOverlay = document.getElementById('overlay');
+const qwerty = document.getElementById('qwerty');
+const phrase = document.getElementById('phrase');
+
+let missed= 0;
+
+
+
+startButton.addEventListener("click", () => {
+  screenOverlay.style.display = 'none';
+});
+
+
+const phrases =[
+'a picture is worth a thousand words',
+'a piece of cake',
+'a good neighbor a found treasure',
+'a penny saved is a penny earned',
+'age is just a number'
+];
+
+
+function getRandomPhraseArray(arr){
+    let newArray = arr[Math.floor(Math.random() * arr.length)];
+    let splitArray = newArray.split('');
+    return splitArray;
+}
+
+
+function addPhraseToDisplay(arr){
+  for (var i = 0; i < arr.length; i++) {
+    const characterArray = arr[i];
+    const ul = document.querySelector('ul');
+    const li = document.createElement('li');
+    li.textContent = characterArray;
+    ul.appendChild(li);
+    if (characterArray === " ") {
+       li.className = "";
+    } else {
+       li.className = "letter";
+    }
+  }
+}
+const phraseArray = getRandomPhraseArray(phrases);
+addPhraseToDisplay(phraseArray);
+
+
+
+
+qwerty.addEventListener("click", (e) => {
+    if (e.target.tagName === 'BUTTON') {
+      let chosenButton = e.target;
+      chosenButton.setAttribute("disabled", '');
+      chosenButton.className = "chosen";
+
+      function targetButton() {
+          let buttonLetters = e.target;
+          return buttonLetters;
+      }
+
+      function checkLetter(button) {
+        const letter = document.getElementsByClassName('letter');
+        for (var i = 0; i < letter.length; i++) {
+        let listLetter = letter[i].textContent;
+        let keyboardButton = button.textContent;
+        let letterFound = letter[i];
+        if (keyboardButton === listLetter) {
+          letterFound.className = "show";
+        } else {
+          return null;
+        }
+       }
+      }
+      checkLetter(targetButton());
+    }
+
+});
